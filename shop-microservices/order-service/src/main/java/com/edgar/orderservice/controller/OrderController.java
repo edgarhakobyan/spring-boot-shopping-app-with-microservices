962e10de204de390cddb9e521a60263b8c498cf9
Edgar Hakobyan <ehakobyan94@gmail.com>
2023-05-11 20:49:53 +0400

    Resilience4J Circuit Breaker

--- shop-microservices/order-service/src/main/java/com/edgar/orderservice/controller/OrderController.java
+++ shop-microservices/order-service/src/main/java/com/edgar/orderservice/controller/OrderController.java
@@ -2,9 +2,14 @@ package com.edgar.orderservice.controller;
 
 import com.edgar.orderservice.dto.OrderRequest;
 import com.edgar.orderservice.service.OrderService;
+import io.github.resilience4j.circuitbreaker.annotation.CircuitBreaker;
+import io.github.resilience4j.timelimiter.annotation.TimeLimiter;
+import io.micrometer.core.annotation.Timed;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.*;
+
+import java.util.concurrent.CompletableFuture;
 
 @RestController
 @RequestMapping("/api/order")
@@ -14,8 +19,13 @@ public class OrderController {
 
     @PostMapping
     @ResponseStatus(HttpStatus.CREATED)
-    public String placeOrder(@RequestBody OrderRequest orderRequest) {
-        orderService.placeOrder(orderRequest);
-        return "Order Placed Successfully";
+    @CircuitBreaker(name = "inventory", fallbackMethod = "orderFallbackMethod")
+    @TimeLimiter(name = "inventory")
+    public CompletableFuture<String> placeOrder(@RequestBody OrderRequest orderRequest) {
+        return CompletableFuture.supplyAsync(() -> orderService.placeOrder(orderRequest));
+    }
+
+    public CompletableFuture<String> orderFallbackMethod(OrderRequest orderRequest, RuntimeException runtimeException) {
+        return CompletableFuture.supplyAsync(() -> "Oops! Something went wrong, please try again later !!!");
     }
 }
