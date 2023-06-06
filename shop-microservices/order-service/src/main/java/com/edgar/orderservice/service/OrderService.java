9a03905df0104e875f951c829b64ce3a658a8ef0
Edgar Hakobyan <ehakobyan94@gmail.com>
2023-06-07 01:16:47 +0400

    Event Driven Architecture using Kafka

--- shop-microservices/order-service/src/main/java/com/edgar/orderservice/service/OrderService.java
+++ shop-microservices/order-service/src/main/java/com/edgar/orderservice/service/OrderService.java
@@ -3,10 +3,12 @@ package com.edgar.orderservice.service;
 import com.edgar.orderservice.dto.InventoryResponse;
 import com.edgar.orderservice.dto.OrderLineItemsDto;
 import com.edgar.orderservice.dto.OrderRequest;
+import com.edgar.orderservice.event.OrderPlacedEvent;
 import com.edgar.orderservice.model.Order;
 import com.edgar.orderservice.model.OrderLineItems;
 import com.edgar.orderservice.repository.OrderRepository;
 import lombok.RequiredArgsConstructor;
+import org.springframework.kafka.core.KafkaTemplate;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 import org.springframework.web.reactive.function.client.WebClient;
@@ -21,6 +23,7 @@ import java.util.UUID;
 public class OrderService {
     private final OrderRepository orderRepository;
     private final WebClient.Builder webClientBuilder;
+    private final KafkaTemplate<String, OrderPlacedEvent> kafkaTemplate;
 
     public String placeOrder(OrderRequest orderRequest) {
         Order order = new Order();
@@ -54,6 +57,7 @@ public class OrderService {
 
         if (allProductsInStock) {
             orderRepository.save(order);
+            kafkaTemplate.send("notificationTopic", new OrderPlacedEvent(order.getOrderNumber()));
             return "Order Placed Successfully";
         } else {
             throw new IllegalArgumentException("Product is not in stock!!!");
